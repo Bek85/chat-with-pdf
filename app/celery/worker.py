364b155307516c04ba213ca7b98da668e2d5e79d
Bekzod Akbarov <stx.webdev@gmail.com>
2025-09-18 18:33:01 +0500

    add runtime error message if REDIS_URI is not set in .env

--- app/celery/worker.py
+++ app/celery/worker.py
@@ -1,4 +1,10 @@
 from app.web import create_app
 
 flask_app = create_app()
+
+if "celery" not in flask_app.extensions:
+    raise RuntimeError(
+        "Celery is not configured. Please set REDIS_URI environment variable."
+    )
+
 celery_app = flask_app.extensions["celery"]
